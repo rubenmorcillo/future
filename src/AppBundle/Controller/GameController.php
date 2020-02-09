ea13e3c77ac6adb56d612c0767f92d97b8c23b04
rubenmorcillo <ravent953@gmail.com>
2020-02-09 21:15:54 +0100

    Creada entidad Distrito + fixtures

--- src/AppBundle/Controller/GameController.php
+++ src/AppBundle/Controller/GameController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Repository\DistrictRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,9 +14,12 @@ class GameController extends Controller
      * @Route("/map", name="mapa_distritos")
      * @Security("is_granted('ROLE_PLAYER')")
      */
-    public function mapAction()
+    public function mapAction(DistrictRepository $districtRepository)
     {
 
-        return $this->render('pruebas/mapa_distritos.html.twig', []);
+        $districts = $districtRepository->findAll();
+        return $this->render('pruebas/mapa_distritos.html.twig', [
+            'districts' => $districts
+        ]);
     }
 }
