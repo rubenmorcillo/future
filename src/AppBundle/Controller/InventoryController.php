90f32589465898ede5b5e3637680b79be9d1f556
rubenmorcillo <ravent953@gmail.com>
2020-02-19 20:09:13 +0100

    Se equipa arma desde el inventario

--- src/AppBundle/Controller/InventoryController.php
+++ src/AppBundle/Controller/InventoryController.php
@@ -2,8 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\PrincipalCharacter;
+use AppBundle\Entity\Weapon;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InventoryController extends Controller
@@ -19,4 +22,28 @@ class InventoryController extends Controller
             'inventory' => $this->getUser()->getInventory()
         ]);
     }
+
+    /**
+     * @Route("/equip/{weapon}/", name="equipar_arma")
+     * @Security("is_granted('ROLE_PLAYER')")
+     */
+    public function equipWeapon(Weapon $weapon){
+
+
+        $principalCharacter = $this->getUser()->getPrincipalCharacter();
+        $oldWeapon = $principalCharacter->getEquipedWeapon();
+        if ($oldWeapon != null){
+            $oldWeapon->setEquiped(null);
+        }
+        $weapon->setEquiped($principalCharacter);
+        $principalCharacter->setEquipedWeapon($weapon);
+
+        $em = $this->getDoctrine()->getManager();
+        try{
+            $em->flush();
+        }catch (\Exception $e){
+        }
+        return $this->redirectToRoute('inventario');
+    }
+
 }
